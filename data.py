985d0c97f1833490c3077b3cdf05ba72f0a95be4
ChangZihao <648639425@qq.com>
2018-07-13 14:19:17 +0800

    first commit

---
+++ data.py
@@ -0,0 +1,17 @@
+import time 
+import random
+
+f =  open('latency.json', 'r+')
+
+while True:
+    #t = int(time.time() * 1000)
+    latency = random.randint(1, 500)
+
+    s = '{"latency": %d}' %(latency)
+    time.sleep(1)
+        
+    f.seek(0)
+    f.write(s)
+    f.truncate()
+
+    #f.write
